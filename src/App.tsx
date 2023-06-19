f26ad66727ca88ad05b15d62ff46098ae083660b
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-06-19 14:54:49 -0300

    estilizacao convertida para styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import TemaClaro from './themes/light'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={TemaClaro}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/themes/light.ts
@@ -0,0 +1,11 @@
+export type Tema = {
+  corPrincipal: string
+  corSecundaria: string
+}
+
+const TemaClaro = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7'
+}
+
+export default TemaClaro
